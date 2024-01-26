bd12244d584451709ffb3c8129ca4044eb931671
Zolastic <honhattienlul@gmail.com>
2024-01-26 11:26:12 +0800

    feat(section 8): finish exercise 10 - 13 on Recursion Problem Set

--- section8RecursionProblemSet/exercise14fib.js
+++ section8RecursionProblemSet/exercise14fib.js
@@ -3,3 +3,22 @@ Write a recursive function called fib which accepts a number and returns the nth
 Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... 
 which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
  */
+
+const fibInterative = (num) => {
+  if (num == 1 || num == 2) return 1;
+  let arr = [1, 1];
+  for (let i = 0; i < num; i++) {
+    arr.push(arr[i] + arr[i + 1]);
+  }
+
+  return arr[num];
+};
+
+console.log(fibInterative(4));
+console.log(fibInterative(10));
+console.log(fibInterative(28));
+console.log(fibInterative(35));
+
+const fib = (num) => {
+  if (num <= 0) return 0;
+};
