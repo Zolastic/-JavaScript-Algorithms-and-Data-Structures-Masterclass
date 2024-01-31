4a4cf38f8c9de8e147b2f67f288ae6edf6305dcb
Zolastic <honhattienlul@gmail.com>
2024-01-31 23:51:07 +0800

    feat(section 13): finish insertion sort

--- section13InsertionSort/insertionSort.js
+++ section13InsertionSort/insertionSort.js
@@ -17,3 +17,18 @@ const insertionSort = (arr) => {
   }
   return arr;
 };
+
+const insertionSort2 = (arr) => {
+  // start by picking the second element in the array
+  for (let i = 1; i < arr.length; i++) {
+    // now compare the second element with the one before it and swap if necessary
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      console.log(arr[i], arr[j]);
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+};
+console.log(insertionSort2([2, 1, 9, 76, 4]));
